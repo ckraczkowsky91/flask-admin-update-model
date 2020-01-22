77ad36f8bf29972908851e0bdcf7357c86ec2700
Colin Thomas Kraczkowsky <Home@Colins-MacBook-Pro-2.local>
2020-01-21 17:33:47 -0800

    Moved List view into homepage of administrative interface

--- app.py
+++ app.py
@@ -10,10 +10,17 @@ class BookView(ModelView):
     column_list = ['title', 'author', 'is_checked_out']
     list_template = 'custom_list.html'
 
+    # def __init__(self, *args, **kwargs):
+    #     super().__init__(*args, **kwargs)
+    #     self.static_folder = 'static'
+
+    def __init__(self, *args, **kwargs):
+        super().__init__(*args, **kwargs)
+        self.static_folder = 'static'
+
     def get_query(self):
         return self.session.query(self.model).filter(self.model.is_checked_out==True)
 
-admin = Admin(app, name='Hogwarts Library', template_mode='bootstrap3')
-admin.add_view(BookView(Book, db.session, name='Check In'))
+admin = Admin(app, name='Hogwarts Library', template_mode='bootstrap3', index_view=BookView(Book, db.session, name='Check In', url='/admin', endpoint='admin'))
 
 from . import routes
